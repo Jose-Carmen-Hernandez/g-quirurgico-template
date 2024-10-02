679765c26fadbd5c9e146861bd4ccbe6d6a530a9
Jose Carmen Hernandez <jcarmenh156@gmail.com>
2024-10-01 21:59:58 -0600

    deploy

---
+++ productos.js
@@ -0,0 +1,310 @@
+const productosBodyTherapy = [
+  {
+    id: 1,
+    nombre: "Depilación definitiva rejuveneciendo la piel (No dolorosa)",
+    img: "./img/grupoQ/tfbc8dwt.png",
+  },
+  {
+    id: 2,
+    nombre: "Rejuvenecimiento facial sin cirugía",
+    img: "./img/grupoQ/inyeccion-facial.webp",
+  },
+  {
+    id: 4,
+    nombre: "Radiofrecuencia",
+    img: "./img/grupoQ/radiofrecuencia.png",
+  },
+  {
+    id: 5,
+    nombre: "Microdermoabrasión",
+    img: "./img/grupoQ/microdermoabrasion.webp",
+  },
+  {
+    id: 6,
+    nombre: "Luz pulsada intensa",
+    img: "./img/grupoQ/g2w2c374.png",
+  },
+  {
+    id: 7,
+    nombre: "Manchas faciales",
+    img: "./img/grupoQ/manchas-faciales.jpeg",
+  },
+  {
+    id: 8,
+    nombre: "Limpieza facial profunda (acné, cicatrices faciales)",
+    img: "./img/grupoQ/acne.jpeg",
+  },
+  {
+    id: 9,
+    nombre: "Rosácea",
+    img: "./img/grupoQ/rosacea.jpeg",
+  },
+  {
+    id: 10,
+    nombre: "Venitas rojas faciales",
+    img: "./img/grupoQ/venitas-rojas-2.png",
+  },
+  {
+    id: 11,
+    nombre: "Armonía facial",
+    img: "./img/grupoQ/armonizacion-facial.jpeg",
+  },
+  {
+    id: 12,
+    nombre: "Dermapen",
+    img: "./img/grupoQ/dermapen.jpeg",
+  },
+  {
+    id: 13,
+    nombre: "Enzimas lipolíticas (Bichectomía enzimática) y perfil facial",
+    img: "./img/grupoQ/bichectomia.jpeg",
+  },
+  {
+    id: 14,
+    nombre: "Eliminación de verrugas con fibroblast (No doloroso)",
+    img: "./img/grupoQ/verrugas.jpeg"
+  },
+  {
+    id: 15,
+    nombre: "Lifting facial y de cuello",
+    img: "./img/grupoQ/lifting-facial.jpeg",
+  },
+  {
+    id: 16,
+    nombre: "Peeling facial",
+    img: "./img/grupoQ/peeling-facial.jpeg",
+  },
+  {
+    id: 17,
+    nombre: "Drenaje linfático facial y corporal",
+    img: "./img/grupoQ/linfatico-facial.jpeg",
+  },
+  {
+    id: 18,
+    nombre: "Lesiones musculares (Recupera la movilidad.)",
+    img: "./img/grupoQ/lesion-muscular.jpeg",
+  },
+  {
+    id: 19,
+    nombre: "Lesiones cutáneas",
+    img: "./img/grupoQ/lesiones-cutaneas.jpeg",
+  },
+  {
+    id: 21,
+    nombre: "Masaje terapéutico descontracturante",
+    img: "./img/grupoQ/masaje-descontracturante.jpeg",
+  },
+  {
+    id: 22,
+    nombre: "Masaje antiestrés",
+    img: "./img/grupoQ/masaje-antiestres.jpeg",
+  },
+  {
+    id: 23,
+    nombre: "Masaje de drenaje linfático",
+    img: "./img/grupoQ/masaje-linfatico.jpeg",
+  },
+  {
+    id: 24,
+    nombre: "Reduce células grasas con aparatología",
+    img: "./img/grupoQ/reduccion-de-grasas.jpeg",
+  },
+  {
+    id: 25,
+    nombre: "Elimina flacidéz, celulitis y estrías",
+    img: "./img/grupoQ/celulitis.jpeg",
+  },
+  {
+    id: 26,
+    nombre: "Moldeo corporal y tonifica",
+    img: "./img/grupoQ/moldeo corporal.jpeg",
+  },
+  {
+    id: 27,
+    nombre: "Reducción de medidas",
+    img: "./img/grupoQ/reduccion-de-medidas.jpeg",
+  },
+  {
+    id: 28,
+    nombre:
+      "Terapias postoperatorias (Abdominoplastías, lipoescultura, cesáreas)",
+    img: "./img/grupoQ/cicatriz-cesarea.jpeg",
+  },
+  {
+    id: 29,
+    nombre: "Calvicie",
+    img: "./img/grupoQ/calvicie.jpeg",
+  },
+  {
+    id: 30,
+    nombre:
+      "Aclara zonas obscuras (Axilas, zona íntima, rodillas, codo, cuello)",
+    img: "./img/grupoQ/codo-oscuro.jpeg",
+  },
+];
+
+const productosMedicinaGeneral = [
+  {
+    id: 31,
+    nombre: "Consulta medica.",
+    img: "./img/grupoQ/medico masculino.webp",
+  },
+  {
+    id: 32,
+    nombre: "Certificados escolares.",
+    img: "./img/grupoQ/certificado-escolar.webp",
+  },
+  {
+    id: 33,
+    nombre: "Toma de glucosa.",
+    img: "./img/grupoQ/toma-de-glucosa.webp",
+  },
+  {
+    id: 34,
+    nombre: "Toma de presión arterial.",
+    img: "./img/grupoQ/presionarterial.webp",
+  },
+  {
+    id: 35,
+    nombre: "Nebulizaciones.",
+    img: "./img/grupoQ/nebulizaciones.webp",
+  },
+  {
+    id: 36,
+    nombre: "Inyecciones.",
+    img: "./img/grupoQ/inyecciones.webp",
+  },
+  {
+    id: 37,
+    nombre: "Control del niño sano.",
+    img: "./img/grupoQ/infantil.webp",
+  },
+  {
+    id: 38,
+    nombre: "Planificación familiar.",
+    img: "./img/grupoQ/prueba de embarazo.webp",
+  },
+  {
+    id: 39,
+    nombre: "Control prenatal.",
+    img: "./img/grupoQ/control-prenatal.webp",
+  },
+  {
+    id: 40,
+    nombre: "Lavado de oídos.",
+    img: "./img/grupoQ/lavado-de-oidos.webp",
+  },
+  {
+    id: 41,
+    nombre: "Teleconsulta.",
+    img: "./img/grupoQ/teleconsulta.webp",
+  },
+
+];
+
+const productosCirugia = [
+  {
+    id: 43,
+    nombre: "Suturas y retiro de puntos.",
+    img: "./img/grupoQ/sutura.webp",
+  },
+  {
+    id: 44,
+    nombre: "Curaciones.",
+    img: "./img/grupoQ/curaciones.webp",
+
+  },
+  {
+    id: 45,
+    nombre: "Colocación, cambio y retiro de sonda foley.",
+    img: "./img/grupoQ/sonda-foley.webp",
+  },
+  {
+    id: 46,
+    nombre: "Laboratorio clínico.",
+    img: "./img/grupoQ/laboratorio-clinico.webp",
+  },
+  {
+    id: 47,
+    nombre:
+      "Prevención, diagnóstico y tratamiento de enfermedades crónico-degenerativas",
+    img: "./img/grupoQ/cronico-corazon.png",
+  },
+  {
+    id: 48,
+    nombre: "Cirugía abdominal, Vesícula, Apéndice.",
+    img: "./img/grupoQ/quirófano.webp"
+  },
+  {
+    id: 49,
+    nombre: "Paracentésis.",
+    img: "./img/grupoQ/paracentesis.webp",
+  },
+  {
+    id: 50,
+    nombre: "Falla intestinal.",
+    img: "./img/grupoQ/falla-intestinal.webp",
+  },
+
+];
+
+const listaProductos = document.getElementById("listaProductos");
+console.log(listaProductos)
+
+/* se elimina href="index.html" para evitar que cada imagen se convirtiera en un enlace */
+productosMedicinaGeneral.map((product, i) => {
+  listaProductos.innerHTML += `<!-- Item -->
+                                  <div class="cbp-item generalMedicine" >
+                                      <a class="cbp-caption" >
+                                          <div class="bg-white shadow-sm rounded-lg overflow-hidden p-1 m-3">
+                                              <div class="cbp-caption-defaultWrap">
+                                                  <img src="${product.img}" width="373" height="298"
+                                                      alt="Image Description">
+                                              </div>
+                                          </div>
+                                          <div class="text-center p-3">
+                                              <span class="d-block h4 mb-0">${product.nombre} </span>
+                                          </div>
+                                      </a>
+                                  </div>
+                                <!-- End Item -->`;
+});
+
+/* se elimina href="index.html" para evitar que cada imagen se convirtiera en un enlace */
+
+productosCirugia.map((product, i) => {
+  listaProductos.innerHTML += `<!-- Item -->
+                                <div class="cbp-item surgery">
+                                    <a class="cbp-caption">
+                                        <div class="bg-white shadow-sm rounded-lg overflow-hidden p-1 m-3">
+                                            <div class="cbp-caption-defaultWrap">
+                                                <img src="${product.img}" width="373" height="298"
+                                                    alt="Image Description">
+                                            </div>
+                                        </div>
+                                        <div class="text-center p-3">
+                                            <span class="d-block h4 mb-0">${product.nombre}</span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <!-- End Item -->`;
+});
+
+/* se mantiene href="index.html" para consultar si es conveniente que cada imagen sea un enlace */
+productosBodyTherapy.map((product, i) => {
+  listaProductos.innerHTML += `<!-- Item -->
+                              <div class="cbp-item bodyTherapy">
+                                  <a class="cbp-caption" > 
+                                      <div class="bg-white shadow-sm rounded-lg overflow-hidden p-1 m-3">
+                                          <div class="cbp-caption-defaultWrap">
+                                              <img src="${product.img}" width="373" height="298"
+                                                  alt="Image Description">
+                                          </div>
+                                      </div>
+                                      <div class="text-center p-3">
+                                          <span class="d-block h4 mb-0">${product.nombre}</span>
+                                      </div>
+                                  </a>
+                              </div>
+                              <!-- End Item -->`;
+});
